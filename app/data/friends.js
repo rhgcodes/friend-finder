35fa49382d1c6d25776206fe7d7eaf713cedac03
rhgcodes <rgalleguillos87@outlook.com>
2018-12-22 23:42:47 -0500

    updated

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends= [
+	{
+		name: "Elizabeth",
+		photo: "https://thumb1.shutterstock.com/display_pic_with_logo/698308/658592416/stock-photo-big-bright-white-smile-headshot-with-a-beautiful-brunette-woman-sincere-happy-cheerful-positive-658592416.jpg",
+		scores: [
+			5,
+			2,
+			5,
+			4,
+			3,
+			1,
+			5,
+			3,
+			4,
+			4
+		]
+	},
+	{
+		name: "Alycia",
+		photo: "https://thumb1.shutterstock.com/display_pic_with_logo/3394091/377261680/stock-photo-closeup-red-head-young-woman-pretty-pinup-girl-green-button-shirt-giving-thumbs-up-sign-gesture-377261680.jpg",
+		scores: [
+			3,
+			4,
+			2,
+			3,
+			3,
+			5,
+			5,
+			1,
+			5,
+			1
+		]
+	},
+	{
+		name: "Michael",
+		photo: "https://thumb1.shutterstock.com/display_pic_with_logo/1849259/706363516/stock-photo-surfer-dude-in-wetsuit-gesturing-on-board-portrait-706363516.jpg",
+		scores: [
+			3,
+			4,
+			4,
+			4,
+			1,
+			2,
+			4,
+			2,
+			3,
+			5
+		]
+	},
+	{
+		name: "Jason",
+		photo: "https://www.stockvault.net/data/2009/10/30/111422/preview16.jpg",
+		scores: [
+			3,
+			2,
+			2,
+			1,
+			3,
+			1,
+			4,
+			1,
+			1,
+			1
+		]
+	},
+	{
+		name: "Wanda",
+		photo: "https://thumb1.shutterstock.com/display_pic_with_logo/160669/384995470/stock-photo-smiling-happy-woman-384995470.jpg",
+		scores: [
+			5,
+			5,
+			5,
+			5,
+			4,
+			4,
+			5,
+			1,
+			1,
+			1
+		]
+	}
+];
+
+module.exports = friends;
